ff546216a9785902f44c2f4bdfec39ca49acea5d
ilqar <mmmdliilqar88@gmail.com>
2023-05-02 00:23:53 +0400

    new

--- Task_01/fibonacci.java
+++ Task_01/fibonacci.java
@@ -1,7 +1,23 @@
 package Task_01;
 
+import java.util.Scanner;
+
 public class fibonacci {
     public static void main(String[] args) {
-        System.out.println("hwshwds");
+        Scanner s = new Scanner(System.in);
+        int numb = s.nextInt();
+        int a = 0, b = 1, c = 0;
+        System.out.println(a);
+        System.out.println(b);
+
+        while (c < numb) {
+            c = a + b;
+            if (c < numb) {
+                System.out.println(c);
+            }
+            a = b;
+            b = c;
+        }
+
     }
 }
