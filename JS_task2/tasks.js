896a471eefd5ee94365df66104c1cfc594f5756c
ilqar <mmmdliilqar88@gmail.com>
2023-06-06 15:31:09 +0400

    new

---
+++ JS_task2/tasks.js
@@ -0,0 +1,153 @@
+//1.Write program that will reverse string: 'Hello world' => 'dlrow olleH'
+
+let a="Hello world";
+let b=[];
+let c=[];
+let d=' '
+ for(let i=0; i<a.length; i++){
+    b.push(a[i]);
+}
+
+console.log(b);
+
+for(let i=b.length-1; i>=0; i--){
+c.push(b[i]);
+}
+console.log(c);
+for(let i=0; i<c.length;i++){
+  d+=c[i];
+}
+console.log(d);
+
+//2.Write program to repeat every character in string 3 times: 'Hello world' => 'HHHeeellllllooo wwwooorrrlllddd!!!'
+
+let e='Hello world';
+let g=[]
+let s='';
+ 
+for(let i=0; i<g.length; i++){
+    s+=g[i];
+}
+console.log(s);
+
+//3.Write program to repeat given string n times: 'Hello world', 3 => 'Hello worldHello worldHello world'
+
+
+let f='Hello world';
+let n=3;
+let  w='';
+
+for (let i=0; i<=n; i++){
+    w+=f;
+}
+
+console.log(w);
+
+
+//9.Create a program to calculate the sum of the elements in an array of numbers: [1, 2, 3, 4, 5] => 15
+let arr= [1, 2, 3, 4, 5];
+let sum=0;
+for(let i=0; i<arr.length; i++){
+sum+=arr[i];
+}
+console.log(sum);
+
+//10.Create a program to calculate the average of the elements in an array of numbers: [1, 2, 3, 4, 5] => 3
+let avg=0;
+for(let i=0; i<arr.length; i++){
+avg=sum/arr.length;
+}
+console.log(avg);
+
+//11.Create a program to find the maximum number in an array of numbers: [1, 2, 3, 4, 5,22,] => 5
+let myArray=[1, 2, 3, 4, 5];
+let myMax=myArray[0];
+
+for (let i=1; i<myArray.length; i++){
+    if(myMax<myArray[i]){
+   myMax=myArray[i];
+    }
+}
+console.log(myMax);
+
+
+//12.Create a program to find the minimum number in an array of numbers: [1, 2, 3, 4, 5] => 1
+let myArray2=[1, 2, 3, 4, 5];
+let myMin=myArray2[0];
+
+for (let i=1; i<myArray2.length; i++){
+    if(myMin>myArray2[i]){
+   myMin=myArray2[i];
+    }
+}
+console.log(myMin);
+
+
+
+//13.Create a program to find the second largest number in an array of numbers: [1, 2, 3, 4, 5] 
+let secondMax =0;
+let myArray3=[1,2,3,4,5,2,44,4,43];
+let num=myArray3.length;
+myArray3 = myArray3.sort((a,b) =>a-b);
+
+secondMax = myArray3[num-2]
+
+console.log(secondMax);
+
+
+
+//14.Create a program to find the second smallest number in an array of numbers: [1, 2, 3, 4, 5] => 2
+
+let secondMin =0;
+let myArray4=[1,2,3,4,5,2,44,4,43];
+myArray4 = myArray4.sort((a,b) =>a-b);
+
+secondMin = myArray4[1]
+
+console.log(secondMin);
+
+
+//15.Create a program to find the count of each element in an array of numbers: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5] => {1: 2, 2: 2, 3: 2, 4: 2, 5: 2}
+
+
+ 
+
+
+
+//16.Create a program to find the sum of the odd numbers in an array of numbers: [1, 2, 3, 4, 5] => 9
+
+myArray=[1, 2, 3, 4, 5];
+let oddSum=0;
+
+for(let i=0; i<myArray.length; i++){
+    if(myArray[i]%2 != 0){
+    oddSum+=myArray[i];
+}
+}
+
+console.log(oddSum);
+
+
+//17.Create a program to find the sum of the even numbers in an array of numbers: [1, 2, 3, 4, 5] => 6
+
+
+myArray=[1, 2, 3, 4, 5];
+evenSum=0;
+
+for(let i=0; i<myArray.length; i++){
+    if(myArray[i]%2 == 0){
+    evenSum+=myArray[i];
+}
+}
+
+console.log(evenSum);
+
+
+
+
+
+
+
+
+
+ 
--- class-task/class-task.js
+++ class-task/class-task.js
@@ -1,8 +1,66 @@
-const asciicode=  [104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100];
-let word='';
-for (let i=0; i<words.length; i++) {
 
-let letter =String.fromCharCode(asciicode[i]);
-word += letter;
+// 2. Write a JavaScript program to convert a string to an array of ASCII codes.
+// example: hello world
+// output: [104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]
+// example: Salam Azerbaycan
+// example: javascript is bomba
+console.log("--------------");
+
+console.log("Task No2");
+let word="javascript is bomba";
+let numberArr=[];
+for(let i=0; i<word.length; i++){
+    numberArr.push(word.charCodeAt(i));
 }
-console.log (word);
+console.log(numberArr);
+
+console.log("--------------");
+
+
+
+//3. Write a JavaScript program to convert a string to an array of characters.
+// example: hello world
+console.log("--------------");
+console.log("Task No3");
+
+let word3="hello world";
+let arr= [];
+for(let i=0; i<word3.length; i++){
+arr.push(word3[i]);
+
+}
+console.log(arr);
+
+console.log("--------------");
+
+
+// 4. Write a JavaScript program to drop the vowels from a string.
+// example: hello world
+// output: hll wrld
+
+console.log("--------------");
+console.log("Task No4");
+debugger
+
+let vowels=['a', 'u', 'e', 'o', 'i' ];
+let word4="hello world";
+let newWord=[];
+let array= [];
+for(let i=0; i<word4.length; i++){
+array.push(word4[i]);
+
+
+}
+
+console.log(array);
+
+for(let i=0; i<array.length; i++) {
+    for(let j=0; j<vowels.length; j++){
+        if(array[i]!=vowels[j]){
+             newWord.push(array[i]);
+        }
+    }
+
+}
+
+console.log("--------------");
